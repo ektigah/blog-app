50cdf2227d85c45be81b77451b9564be559887f2
ektigah <ektigah@gmail.com>
2019-01-05 02:43:50 -0500

    Create users table model

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+ 
+  root 'welcome#home'
+  get 'about', to: 'welcome#about'
+  
+  resources :articles
+  
+  get 'signup', to: "users#new"
+  resources :users, except: [:new]
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
+  
+  resources :categories, except: [:destroy]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
