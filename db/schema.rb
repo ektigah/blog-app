3ccd75b2d97db2e1ec515e987860e3c9c063202a
ektigah <ektigah@gmail.com>
2019-01-12 10:19:51 -0500

    changing to sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2019_01_11_071112) do
 
-  create_table "article_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
     t.integer "category_id"
   end
 
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "picture"
@@ -26,13 +26,13 @@ ActiveRecord::Schema.define(version: 2019_01_11_071112) do
     t.integer "user_id"
   end
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "comments", force: :cascade do |t|
     t.string "website"
     t.string "email"
     t.string "author_name"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2019_01_11_071112) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
